ce4a0e17a9531edd099091f93310979e21d6c12a
Brandon Martino <167354702+bmartino1@users.noreply.github.com>
2026-03-05 18:17:46 -0600

    Update 01-dependencies.sh

--- build/01-dependencies.sh
+++ build/01-dependencies.sh
@@ -17,7 +17,7 @@ apt-get install -y --no-install-recommends \
   python3 python3-pip python3-venv python3-dev \
   build-essential git jq \
   certbot python3-certbot-nginx \
-  iproute2 procps
+  iproute2 procps mc lua-inspect
 
 echo "==> [01] Installing Nginx with stream module..."
 apt-get install -y nginx libnginx-mod-stream
